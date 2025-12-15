833802932e7e67b0fb6121522d5e4a788550426b
hidden9-9 <hidden0799@gmail.com>
2025-12-15 13:33:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿function login(){
+ localStorage.setItem('user',name.value);
+ location.href='shop.html';
+}
+
+function viewProduct(name,price){
+ localStorage.setItem('product',name);
+ localStorage.setItem('price',price);
+ location.href='product.html';
+}
+
+if(document.getElementById('pname')){
+ document.getElementById('pname').innerText=localStorage.getItem('product');
+}
+
+function addToCart(){
+ alert('Added to cart');
+}
+
+function confirmOrder(){
+ alert('Payment Successful');
+ location.href='confirm.html';
+}
